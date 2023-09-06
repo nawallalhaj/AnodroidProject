e7ff8df65e0b4a66fc4cffac88204ebec083c98e
omaroium <96143894+omaroium@users.noreply.github.com>
2023-09-06 14:24:18 +0300

    User

---
+++ app/src/main/java/com/example/nawalproj/Classes/User.java
@@ -0,0 +1,10 @@
+package com.example.nawalproj.Classes;
+
+public class User {
+    private String email;
+    private String password;
+    public User(String email, String password){
+        this.email=email;
+        this.password=password;
+    }
+}
